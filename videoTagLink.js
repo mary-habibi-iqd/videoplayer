233d9702d42713570b10fa80229edb18ca1aae54
Mary Habibi <marzieh.habibi@iqdigital.de>
2024-01-15 17:05:32 +0100

    init videoplayer project

---
+++ videoTagLink.js
@@ -0,0 +1,13 @@
+const link =
+  `https://pubads.g.doubleclick.net/gampad/ads?
+  iu=/183/ariva/videoplayer&
+  description_url=http%3A%2F%2Fgoogle.com&
+  tfcd=0&npa=0&
+  sz=16x9&
+  gdfp_req=1&
+  kw=test_mary_ulf&
+  output=vast&
+  env=vp&
+  unviewed_position_start=1&
+  impl=s&
+  correlator=`
